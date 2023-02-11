52c346f7cc98bf21235e8c2dd99d3e0240261b22
SongJeKang <123530209+SongJeKang@users.noreply.github.com>
2023-02-11 14:28:23 +0900

    Add files via upload

---
+++ boj/2023_02_11/1105_8.py
@@ -0,0 +1,18 @@
+import sys
+
+input=sys.stdin.readline().strip
+
+L,R=input().split()
+ans=0
+if len(L)!=len(R):
+    print(0)
+else:
+    for i in range(len(L)):
+        if L[i]==R[i]:
+            if L[i]=="8":
+                ans+=1
+        else:
+            break
+print(ans)
+
+
